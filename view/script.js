d4ca5a532f2b540c744999a144be54d51f6f017b
junyou <junyou1998@gmail.com>
2024-01-03 17:51:29 +0800

    add frontend ui and some function

--- view/script.js
+++ view/script.js
@@ -1,30 +1,58 @@
-const API_URL = "http://127.0.0.1:5038";
+const API_URL = "http://192.168.0.54:5038";
 const { ref, onMounted } = Vue;
 
 const App = {
     setup() {
         const tasks = ref([]);
+
+        const addShow = ref(false);
+        // Task CRUD
         const refreshData = async () => {
             axios.get(API_URL + "/api/tasks").then((res) => {
                 tasks.value = res.data;
             });
         };
-        const addNewTask = async () => {
+
+        let enterPressed = false;
+        const addNewTask = async (event) => {
+
+            if (event.type === "keyup" && event.key === "Enter") {
+                enterPressed = true;
+                event.target.blur();
+            }
+
+            if (event.type === "blur" && enterPressed) {
+                enterPressed = false; 
+                return;
+            }
+
             let newTasks = document.querySelector("#newTask").value;
-            const formData = new FormData();
-            formData.append("newTasks", newTasks);
 
-            axios.post(API_URL + "/api/tasks", formData).then((res) => {
-                refreshData();
-                alert(res.data);
+            if (newTasks != "") {
+                const formData = new FormData();
+                formData.append("newTasks", newTasks);
 
-                console.log("add successful");
-            });
+                axios.post(API_URL + "/api/tasks", formData).then((res) => {
+                    refreshData();
+                    // alert(res.data);
+
+                    console.log("add successful");
+
+                    // 清空輸入框
+                    event.target.value = "";
+
+                    enterPressed = false;
+                });
+            } else {
+                console.log("no data");
+            }
+            addShow.value = false;
         };
         const deleteTask = async (_id) => {
+            navigator.vibrate(10);
             axios.delete(API_URL + "/api/tasks/" + _id).then((res) => {
                 refreshData();
-                alert(res.data);
+                // alert(res.data);
                 console.log("delete successful");
             });
         };
@@ -33,16 +61,23 @@ const App = {
                 .patch(API_URL + "/api/tasks/" + _id, { updateTask: updateValue, doneStatus: status })
                 .then((res) => {
                     refreshData();
-                    alert(res.data);
+                    // alert(res.data);
                     console.log("update successful");
                 })
                 .catch((err) => {
                     console.error("error updating task", err);
                 });
         };
+        const showInput = () => {
+            navigator.vibrate(10);
+            addShow.value = !addShow.value;
+            Vue.nextTick(() => {
+                document.querySelector("#newTask").focus();
+            });
+        };
 
         onMounted(refreshData);
-        return { tasks, addNewTask, deleteTask, updateTask };
+        return { tasks, addNewTask, deleteTask, updateTask, addShow, showInput };
     },
 };
 
